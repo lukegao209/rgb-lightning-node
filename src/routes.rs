ef9c34db77e5183551ee1c72a8cea2e5f4a9224d
ka <ka@ln.exchange>
2025-06-13 19:54:08 +0800

    add address field to /listpeers response

--- src/routes.rs
+++ src/routes.rs
@@ -826,6 +826,7 @@ pub(crate) struct Payment {
 #[derive(Clone, Deserialize, Serialize)]
 pub(crate) struct Peer {
     pub(crate) pubkey: String,
+    pub(crate) address: String,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -2143,6 +2144,7 @@ pub(crate) async fn list_peers(
     for peer_details in unlocked_state.peer_manager.list_peers() {
         peers.push(Peer {
             pubkey: peer_details.counterparty_node_id.to_string(),
+            address: peer_details.socket_address.map(|addr| addr.to_string()).unwrap_or_else(|| "unknown".to_string()),
         })
     }
 
